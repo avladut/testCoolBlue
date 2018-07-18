2e65506c272fa3a56aaa12fb3bfb53afdb250c1a
Alexandru Vladut <Alexandru.Vladut@endava.com>
2018-07-18 09:47:06 +0300

    renamed view model class. refactored error manager

---
+++ CoolBlueTest/Screens/SearchScreen/ViewModels/ProductListViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ProductListViewModel.swift
+//  CoolBlueTest
+//
+//  Created by Alexandru Vladut on 18/07/2018.
+//  Copyright © 2018 Alexandru Vladut. All rights reserved.
+//
+
+import Foundation
